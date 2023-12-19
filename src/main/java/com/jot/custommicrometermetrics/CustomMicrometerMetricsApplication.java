ecbafe3f222579ecb1c52ddf8e275fc5f139309b
Ernie <superernie77@gmail.com>
2023-12-19 10:55:07 +0100

    project init

---
+++ src/main/java/com/jot/custommicrometermetrics/CustomMicrometerMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.jot.custommicrometermetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomMicrometerMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomMicrometerMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jot/custommicrometermetrics/CustomMicrometerMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jot.custommicrometermetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomMicrometerMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
